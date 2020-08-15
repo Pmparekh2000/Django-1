83e29e1b787a5cb403be35467a37464c75e3a88b
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-08-15 09:35:24 -0700

    Fetched data from backend and added css

--- todolist/views.py
+++ todolist/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+import logging
+
+from .models import Todolist
 
 # Create your views here.
 
+logger = logging.getLogger(__name__)
+
 def index(request):
-    return render(request, "todolist/index.html")
+
+    todo_items = Todolist.objects.order_by('id')
+
+    context = {
+        'todo_items': todo_items
+    }
+    logger.error(todo_items)
+    return render(request, "todolist/index.html", context)
 
 
